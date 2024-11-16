d031c2b77865c4b37ee6998b7c73cf8244517ac2
skyh24 <skyhighfeng@gmail.com>
2024-11-16 19:40:07 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,9 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-const privateKey = "PRIVATE_KEY";
-const account = privateKeyToAccount(`0x${privateKey}`)
+const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const account = privateKeyToAccount(`${privateKey}`)
 const bundlerUrl = "https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44"
- 
-app.get('/address/:handle', async (c) => {
-  const handle = c.req.param("handle")
-
-  return c.json({ address: await getNexusAddress(handle)
-  // return c.json({ address: handle })
-})
 
 async function getNexusAddress(handle: string) {
   let index = ConvertToBn(handle)
@@ -38,19 +31,22 @@ function ConvertToBn(handle: string) {
   return BigInt(handle)
 }
 
-app.post('/deploy', (c) => {
-  return c.json({ address: getNexusAddress("111") })
+app.get('/address/:handle', async (c) => {
+  const handle = c.req.param("handle")
+
+  // return c.json({ address: await getNexusAddress(handle)
+  return c.json({ address: handle })
+})
+
+
+app.post('/deploy', async (c) => {
+  return c.json({ address: await getNexusAddress("111") })
   // TODO: 组 deploy , 发一笔空交易（或 changeOwner）
 })
 
-app.post('/changeOwner',  (c) => {
-  return c.json({ address: getNexusAddress("111") })
+app.post('/changeOwner',  async (c) => {
+  return c.json({ address: await getNexusAddress("111") })
   // TODO: 组 changeOwner 的逻辑
 })
-
-
- 
-
-
 
 export default app
